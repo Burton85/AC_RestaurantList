911633a0b40ca3ef43242bbb92eccf513952576d
Burton85 <burton00tw@gmail.com>
2019-11-13 01:25:22 +0800

    import MongoDB as DataBase

--- app.js
+++ app.js
@@ -2,38 +2,67 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const restaurantlist = require("./restaurant.json");
 const exphbs = require("express-handlebars");
-
 //setting handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 //use static files
 app.use(express.static("public"));
 
+//setting mongoose
+const mongoose = require("mongoose");
+const RestaurantDB = require("./models/restaurant.js");
+mongoose.connect("mongodb://127.0.0.1/Restaurant", { useNewUrlParser: true });
+db = mongoose.connection;
+
+db.on("error", () => {
+  console.log("db error");
+});
+db.once("open", () => {
+  console.log("db connected");
+});
 //setting routes
 app.get("/", (req, res) => {
-  const rslist = restaurantlist.results;
-  res.render(`index`, { restaurants: rslist });
+  RestaurantDB.find((err, restaurants) => {
+    if (err) return console.log("err");
+    return res.render(`index`, { restaurants: restaurants });
+  });
 });
 //setting search bar
 app.get("/search", (req, res) => {
   const keywords = req.query.keyword;
-  const rsresult = restaurantlist.results.filter(item => {
-    return (
-      item.name.toLowerCase().includes(keywords.toLowerCase()) ||
-      item.name_en.toLowerCase().includes(keywords.toLowerCase())
-    );
+  RestaurantDB.find((err, restaurants) => {
+    if (err) return console.log("err");
+    const retaurantsResualt = restaurants.filter(item => {
+      return (
+        item.name.toLowerCase().includes(keywords.toLowerCase()) ||
+        item.name_en.toLowerCase().includes(keywords.toLowerCase())
+      );
+    });
+    return res.render(`index`, { restaurants: retaurantsResualt });
   });
+  // const rsresult = restaurantlist.results.filter(item => {
+  //   return (
+  //     item.name.toLowerCase().includes(keywords.toLowerCase()) ||
+  //     item.name_en.toLowerCase().includes(keywords.toLowerCase())
+  //   );
+  // });
 
-  res.render("index", { restaurants: rsresult, keywords: keywords });
+  // res.render("index", { restaurants: rsresult, keywords: keywords });
 });
 //go to show page
 app.get("/restaurants/:id", (req, res) => {
-  const restaurant = restaurantlist.results.filter(
-    item => item.id.toString() === req.params.id
-  );
-  res.render("show", { restaurant: restaurant[0] });
+  RestaurantDB.find((err, restaurants) => {
+    if (err) return console.log("error");
+    const restaurantsResults = restaurants.filter(
+      item => item.id.toString() === req.params.id
+    );
+    res.render("show", { restaurant: restaurantsResults[0] });
+  });
+  // const restaurant = restaurantlist.results.filter(
+  //   item => item.id.toString() === req.params.id
+  // );
+  // res.render("show", { restaurant: restaurant[0] });
 });
 
 app.listen(port, () => {
---
+++ models/seeds/restaurantseeder.js
@@ -0,0 +1,37 @@
+//import modules
+const mongoose = require("mongoose");
+const RestaurantDB = require("../restaurant.js");
+const restaurantJson = require("../restaurant.json");
+const results = restaurantJson.results;
+//connect with Database
+mongoose.connect("mongodb://127.0.0.1/Restaurant", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const db = mongoose.connection;
+
+db.on("error", () => {
+  console.log("db error");
+});
+
+db.once("open", () => {
+  console.log("db connected");
+  // for (let i = 0; i < 10; i++) {
+  //   RestaurantDB.create({ name: "name:-" + i });
+  // }
+  for (let i = 0; i < results.length; i++) {
+    RestaurantDB.create({
+      id: results[i].id,
+      name: results[i].name,
+      name_en: results[i].name_en,
+      category: results[i].category,
+      image: results[i].image,
+      location: results[i].location,
+      phone: results[i].phone,
+      google_map: results[i].google_map,
+      rating: results[i].rating,
+      description: results[i].description
+    });
+  }
+});
+console.log("done");
